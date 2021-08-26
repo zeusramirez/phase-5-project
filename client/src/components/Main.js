2c87ec9a4eef9e9ffbe8fb4648f328b5bb86f0a8
zeus <zeus.ramirez347@gmail.com>
2021-08-26 18:08:18 -0400

    Final presentation

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -17,8 +17,12 @@ export default function Main(props) {
     }, [])
     return (
         <div>
-            <h2>Welcome to ProjectBuild</h2>
-            <p>Where car enthusiasts can come together and share their projects and car builds!</p>
+            <br/>
+            <br/>
+            <h1>Welcome to ProjectBuild</h1>
+            <h4>Where the car community can come together and share their projects and car builds.</h4>
+            <br/>
+            <br/>
                 {user && followFeed.length > 0 ? (<><h2>Followed Projects</h2> <div className="page-content">{followFeed.sort(() => Math.random() - 0.5).map(vehicle => <FeedCard key={vehicle.id} {...vehicle}/>)}</div><h2>All Projects</h2></>):null}
                 
             <div className="page-content">
