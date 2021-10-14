8a91a0d1d8e8034e34ef060085c0483feedcf74e
zeus <zeus.ramirez347@gmail.com>
2021-10-13 23:12:42 -0400

    Streamline code routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
-  # added routes
+  # added new routes
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
  
