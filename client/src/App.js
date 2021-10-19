b46ae97f7f046007ba0c1f4879772f8e9b271eb1
zeus <zeus.ramirez347@gmail.com>
2021-10-18 23:27:05 -0400

    removed unneccesary code.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React, {useState, useEffect} from 'react';
 import {Route, Switch, useHistory, Redirect} from 'react-router-dom'
